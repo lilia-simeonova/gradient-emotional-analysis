bd065e6c8f38e62678d9736011126ec7118ea2bf
lilia.simeonova <lilia.valentinova.simeonova@gmail.com>
2017-07-17 14:19:27 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+
+import { CoordinatesService } from './coordinates.service'
 import { AppComponent } from './app.component';
+import { BotalikeComponent } from './botalike/botalike.component';
+import { TwitterFeedComponent } from './twitter-feed/twitter-feed.component';
+import { TwitterService } from './twitter.service';
+import { ChartComponent } from './chart/chart.component'
+import { ChartsModule } from 'ng2-charts';
+import { NouisliderModule } from 'ng2-nouislider';
+import { CustomComponent } from './custom/custom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotalikeComponent,
+    TwitterFeedComponent,
+    ChartComponent,
+    CustomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    ChartsModule,
+    NouisliderModule
   ],
-  providers: [],
+  providers: [TwitterService, CoordinatesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
